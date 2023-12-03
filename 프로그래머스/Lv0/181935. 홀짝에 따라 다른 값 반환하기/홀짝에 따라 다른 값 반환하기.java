6d979d790aa148436b412d77c126536fad985ce5
rlozlr <143188171+rlozlr@users.noreply.github.com>
2023-12-04 01:18:00 +0900

    without method

--- 프로그래머스/Lv0/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.java
+++ 프로그래머스/Lv0/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.java
@@ -3,35 +3,14 @@ class Solution {
         int answer = 0;
         
         if( n % 2 == 1) {
-            answer = odd(n);
+            for(int i = 1; i <= n; i += 2) {
+                answer += i ;
+            }
         } else {
-            answer = even(n);
+            for(int i = 2; i <= n; i += 2) {
+                answer += i * i;
+            }
         }
-        
         return answer;
-    }
-    
-    // 홀수
-    public int odd(int n) {
-        int k = 0;
-        
-        for( int i = 1; i <= n; i++) {
-            if( i % 2 == 1) {
-                k += i;
-            }
-        }
-        return k;
-    }
-    
-    // 짝수
-    public int even(int n) {
-        int k = 0;
-        
-        for( int i = 1; i <= n; i++) {
-            if( i % 2 == 0) {
-                k += i*i;
-            }
-        }
-        return k;
     }
 }
