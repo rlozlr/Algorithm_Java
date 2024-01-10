70a76f99fb7623a9b0641e203be837c041462734
신은지 <143188171+rlozlr@users.noreply.github.com>
2024-01-10 12:05:57 +0900

    [level unrated] Title: 가장 큰 수 찾기, Time: 0.01 ms, Memory: 72.6 MB -BaekjoonHub

--- 프로그래머스/unrated/120899. 가장 큰 수 찾기/가장 큰 수 찾기.java
+++ 프로그래머스/unrated/120899. 가장 큰 수 찾기/가장 큰 수 찾기.java
@@ -1,8 +1,8 @@
 class Solution {
     public int[] solution(int[] array) {
-        int[] answer = {0, 0};
+        int[] answer = new int [2];
         for(int i = 0; i < array.length; i++) {
-            if(answer[0] < array[i]) {
+            if(array[i] > answer[0]) {
                 answer[0] = array[i];
                 answer[1] = i;
             }
