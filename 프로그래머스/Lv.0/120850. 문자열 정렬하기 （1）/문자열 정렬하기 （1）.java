68ba93461ca63cf618794a810d2afe605827765a
신은지 <143188171+rlozlr@users.noreply.github.com>
2024-01-11 23:57:59 +0900

    [level Lv.0] Title: 문자열 정렬하기 (1), Time: 0.53 ms, Memory: 68 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String my_string) {
+        my_string = my_string.replaceAll("[a-z]","");
+        String[] strArr = my_string.split("");
+        int[] answer = new int [strArr.length];
+        
+        for(int i = 0; i < answer.length; i++) {
+            answer[i] = Integer.parseInt(strArr[i]);
+        }
+        
+        Arrays.sort(answer);
+        
+        return answer;
+    }
+}
