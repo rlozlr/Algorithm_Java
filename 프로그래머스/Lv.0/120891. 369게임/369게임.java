578dfee1f3640e297661a90b59f3186ccba50afb
신은지 <143188171+rlozlr@users.noreply.github.com>
2024-01-15 12:16:30 +0900

    [level Lv.0] Title: 369게임, Time: 0.04 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/Lv.0/120891. 369게임/369게임.java
+++ 프로그래머스/Lv.0/120891. 369게임/369게임.java
@@ -2,14 +2,13 @@ class Solution {
     public int solution(int order) {
         int answer = 0;
         String str = String.valueOf(order);
-        int count  = 0;
         
         for(int i = 0; i < str.length(); i++) {
             if(str.charAt(i) == '3' || str.charAt(i) == '6' || str.charAt(i) == '9' ) {
-                count ++;
+                answer++;
             }    
         }
         
-        return answer = count;
+        return answer;
     }
 }
